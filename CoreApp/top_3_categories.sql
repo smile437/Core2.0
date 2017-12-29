dfcbce508e535480780505c09443fd3808e3f4d1
Oleg <kravaoleg84@ukr.net>
2017-12-29 14:24:32 +0200

    add folder with sql queries

---
+++ CoreApp/top_3_categories.sql
@@ -0,0 +1,19 @@
+﻿use ProductDb 
+go 
+
+select top 3 c.*, iq.amount, iq.mean 
+from 
+(
+	select top 3 c.Code, count(p.Code) as [amount], avg(p.Price) as [mean]
+	from Categories c 
+	inner join ProductCategory pc 
+	on pc.CategoryCode = c.Code 
+	inner join Products p 
+	on pc.ProductCode = p.Code 
+	and p.IsAvailable = 1
+	group by c.Code
+) iq
+inner join Categories c 
+on c.Code = iq.Code 
+order by iq.mean desc 
+go 
---
+++ CoreApp/unavailable_yet_to_deliver_in_current_month.sql
@@ -0,0 +1,17 @@
+﻿use ProductDb
+go
+
+Select p.*
+From Products as p
+Where p.IsAvailable=0
+And p.DeliveryDate > convert
+(
+    datetime2,
+    right('000' + convert(varchar(255), datepart(year, getdate())), 4) + '-' + right('0' + convert(varchar(255), datepart(month, getdate())), 2) + '-01 00:00:00'
+) 
+and p.DeliveryDate < convert
+(
+    datetime2,
+    right('000' + convert(varchar(255), datepart(year, dateadd(month, 1, getdate()))), 4) + '-' + right('0' + convert(varchar(255), datepart(month, dateadd(month, 1, getdate()))), 2) + '-01 00:00:00'
+) 
+go 
